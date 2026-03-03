49f1756ffff04ecd7c0d942ababf310ebc140acb
Barenque-700 <barbieri.andrea700@gmail.com>
2026-03-03 09:23:42 +0100

    Create Asteria.php
    Prima base su come funziona il bootstrap

---
+++ Asteria.php
@@ -0,0 +1,19 @@
+<html> 
+	<head>
+		 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+		<title> Asteria </title>
+	</head>
+	<body>
+		<h1> Mega social network pazzurdo per astronomia</h1>
+		<div class="p-3 text-secondary-emphasis bg-secondary-subtle border border-secondary-subtle rounded-3">
+  			Example element with utilities
+		</div>
+		<br>
+		<div class="p-3 text-success-emphasis bg-info-subtle border border-warning-subtle rounded-3"> 
+				Header 
+		</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+	</body>
+</html>

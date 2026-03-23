5fcfb1b417ee6971a7bcd902386d0b3d0746353b
Adday Yasser <adday.yasser7@gmail.com>
2026-03-23 09:50:03 +0100

    Add files via upload

--- Asteria.php
+++ Asteria.php
@@ -4,12 +4,13 @@
 		<title> Asteria </title>
 	</head>
 	<body>
-		 <nav class="navbar navbar-expand-lg bg-light">
-    		<span class="navbar-brand mb-0 h1">Asteria - Social Network</span>
-    		<a class="navbar-brand mx-auto">
-      			<img src="logo.svg" width="30" height="30" alt="">
-    		</a>
-    		<span class="navbar-brand mb-0 h1">Jan. 1, 2020 - Jan. 2, 2020</span>
+	<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid"> <a class="navbar-brand me-auto" href="#"> <img src="logo.png" width="255" height="150" alt="Logo Asteria">
+        </a>
+        <span id="current-date" class="navbar-brand mb-0 h1">
+    </span>
+    </div>
+</nav>
   </nav>
 		<h1> Mega social network pazzurdo per astronomia</h1>
 		<div class="p-3 text-secondary-emphasis bg-secondary-subtle border border-secondary-subtle rounded-3">
@@ -20,6 +21,12 @@
 				Header 
 		</div>
 
+		<script>
+    	const oggi = new Date();
+    	const opzioni = { year: 'numeric', month: 'long', day: 'numeric' };
+    	const dataFormattata = oggi.toLocaleDateString('it-IT', opzioni);
+    	document.getElementById('current-date').textContent = dataFormattata;
+	</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 	</body>

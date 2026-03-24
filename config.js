cd16ee691103a08db6e7864b62c46068ec9e632b
Adday Yasser <adday.yasser7@gmail.com>
2026-03-24 09:47:18 +0100

    Configurazione js della frontpage

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Gestione Data
+const oggi = new Date();
+const opzioni = { day: '2-digit', month: 'long', year: 'numeric' };
+document.getElementById('current-date').textContent = oggi.toLocaleDateString('it-IT', opzioni).toUpperCase();
+
+// 2. Gestione Dark/Light Mode
+const toggleBtn = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+    if (body.getAttribute('data-theme') === 'dark') {
+        body.setAttribute('data-theme', 'light');
+        toggleBtn.innerHTML = "☀️ Modalità Giorno";
+    } else {
+        body.setAttribute('data-theme', 'dark');
+        toggleBtn.innerHTML = "🌙 Modalità Notte";
+    }
+});

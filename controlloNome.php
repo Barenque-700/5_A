c7814c6a3318aa6f022136d580ff62f5b55d4624
Barenque-700 <barbieri.andrea700@gmail.com>
2026-04-21 09:50:14 +0200

    controllo nome utente

---
+++ controlloNome.php
@@ -0,0 +1,42 @@
+<?php
+session_start(); 
+include "Connessione.php";
+$q = $_REQUEST["q"];
+$indicatore = "";
+
+try {
+        $connessione = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+        $sql= 'SELECT NomeUtente 
+            FROM utenti';
+
+        $preparata = $connessione->prepare($sql);
+        $preparata->execute();
+
+        if($preparata->rowCount() > 0){
+            $ris = $preparata->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($q !== "") {
+			    $len=strlen($q);
+			    foreach($ris as $name) {
+			    	foreach ($name as $stringa) {
+			        if (strcmp($q, $stringa)== 0){
+			                $indicatore= 0;
+			                break;
+			            } else {
+			                $indicatore="";
+			            }
+			        }
+			    }
+			}
+		}
+	    $connessione = null;
+    } catch(PDOException $e){
+        die("Errore nella gestione del database $db: " . $e->getMessage());
+    }
+
+echo $indicatore === 0 ? "Il nome utente è già utilizzato"."<br>"."<br>" : "";
+
+
+
+
+?>

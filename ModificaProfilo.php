644e222c284072d4174edc1c6ecdb6b31c198e90
Barenque-700 <barbieri.andrea700@gmail.com>
2026-04-21 09:50:46 +0200

    controllo nome utente

--- ModificaProfilo.php
+++ ModificaProfilo.php
@@ -1,79 +1,101 @@
 <html>
 <link rel="stylesheet" href="StileModifica.css">
-    <div class="main">
-        <div class="title-wrapper">
-            <div class="title">
-                <h1>Modifica Profilo</h1>
-            </div>
-            <div class="wrapper">
-                <div class="box left">      
-                    <form action="ModificaProfilo2.php" method="POST">
-                        <?php 
-                        session_start();
-                        include "Connessione.php";
-                        $NomeUtente = $_SESSION['nomeutente'];
-                        if(isset($NomeUtente)){
-                            try{
-                                $connessione = new PDO("mysql:host=$host;dbname=$db", $user, $password);
-                                $sql= "SELECT Nome, Cognome, NomeUtente, Descrizione, DataNascita, Foto, Password
-                                        FROM utenti
-                                        WHERE NomeUtente=?";
-                                $preparata = $connessione->prepare($sql);
-                                $preparata->execute([$NomeUtente]);
-                                if($preparata->rowCount() > 0){
-                                    $ris = $preparata->fetchAll(PDO::FETCH_ASSOC);
-                                    foreach ($ris as $riga) {
-                        ?>
-                        <div class="form-row">
-                            <div class="form-group">
-                                <label for="nome">Nome:</label><br>
-                                <input type="text" name="nome" value="<?=$riga['Nome']?>">
+<?php
+    session_start();
+    $NomeUtente = $_SESSION['nomeutente'];
+?>
+<script>
+            function controlloNome(str) {
+                if (str.length == 0 || str== "<?=$NomeUtente?>") {
+                    document.getElementById("indicatore").innerHTML = "";
+                    return;
+                } else {
+                    var xmlhttp = new XMLHttpRequest();
+                    xmlhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                            document.getElementById("indicatore").innerHTML = this.responseText;
+                        }
+                    };
+                    xmlhttp.open("GET", "controlloNome.php?q=" + str, true);
+                    xmlhttp.send();
+                }
+            }
+    </script>
+    <body>
+        <div class="main">
+            <div class="title-wrapper">
+                <div class="title">
+                    <h1>Modifica Profilo</h1>
+                </div>
+                <div class="wrapper">
+                    <div class="box left">      
+                        <form action="ModificaProfilo2.php" method="POST">
+                            <?php 
+                            include "Connessione.php";
+                            if(isset($NomeUtente)){
+                                try{
+                                    $connessione = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+                                    $sql= "SELECT Nome, Cognome, NomeUtente, Descrizione, DataNascita, Foto, Password
+                                            FROM utenti
+                                            WHERE NomeUtente=?";
+                                    $preparata = $connessione->prepare($sql);
+                                    $preparata->execute([$NomeUtente]);
+                                    if($preparata->rowCount() > 0){
+                                        $ris = $preparata->fetchAll(PDO::FETCH_ASSOC);
+                                        foreach ($ris as $riga) {
+                            ?>
+                            <div class="form-row">
+                                <div class="form-group">
+                                    <label for="nome">Nome:</label><br>
+                                    <input type="text" name="nome" value="<?=$riga['Nome']?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="cognome">Cognome:</label><br>
+                                    <input type="text" name="cognome" value="<?=$riga['Cognome']?>">
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="cognome">Cognome:</label><br>
-                                <input type="text" name="cognome" value="<?=$riga['Cognome']?>">
-                            </div>
+
+                            <label for="utente">Nome Utente:</label><br>
+                            <input type="text" name="utente" value="<?=$riga['NomeUtente']?>" onkeyup="controlloNome(this.value)"><br/><br/>
+                            <a id="indicatore" class="indicatore"> </a>
+
+                            <label for="nascita">Data di Nascita:</label><br>
+                            <input type="date" name="data" value="<?=$riga['DataNascita']?>"><br/><br/>
+
+                            <label for="descrizione">Descrizione:</label><br>
+                            <input type="text" name="descrizione" value="<?=$riga['Descrizione']?>"><br/><br/>
+
+                            <input type="submit" value="Conferma Modifiche">
+                        </form>
+                    </div>
+                    <div class="box right">
+                        <div class="alto">
+                            <form action="CambiaFoto.php" method="POST" class="formAlto">
+                                <img src="UploadProfili/<?=$riga['Foto']?>" width="150" height="150" alt="FotoProfilo">
+                                <input type="submit" value="Cambia Foto Profilo">
+                            </form>
                         </div>
-
-                        <label for="utente">Nome Utente:</label><br>
-                        <input type="text" name="utente" value="<?=$riga['NomeUtente']?>"><br/><br/>
-
-                        <label for="nascita">Data di Nascita:</label><br>
-                        <input type="date" name="data" value="<?=$riga['DataNascita']?>"><br/><br/>
-
-                        <label for="descrizione">Descrizione:</label><br>
-                        <input type="text" name="descrizione" value="<?=$riga['Descrizione']?>"><br/><br/>
-
-                        <input type="submit" value="Conferma Modifiche">
-                    </form>
-                </div>
-                <div class="box right">
-                    <div class="alto">
-                        <form action="CambiaFoto.php" method="POST" class="formAlto">
-                            <img src="UploadProfili/<?=$riga['Foto']?>" width="150" height="150" alt="FotoProfilo">
-                            <input type="submit" value="Cambia Foto Profilo">
-                        </form>
-                    </div>
-                    <div class="basso">
-                        <form action="CambiaPassword.php" method="POST" class="formBasso">
-                            <label for="password">Inserisci la tua attuale password:</label><br>
-                            <input type="password" name="password"><br/><br/>
-                            <label for="passwordNuova">Nuova Password:</label><br>
-                            <input type="password" name="passwordNuova"><br/><br/>
-                            <input type="submit" value="Cambia Password">
-                        </form>
+                        <div class="basso">
+                            <form action="CambiaPassword.php" method="POST" class="formBasso">
+                                <label for="password">Inserisci la tua attuale password:</label><br>
+                                <input type="password" name="password"><br/><br/>
+                                <label for="passwordNuova">Nuova Password:</label><br>
+                                <input type="password" name="passwordNuova"><br/><br/>
+                                <input type="submit" value="Cambia Password">
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <?php
+        <?php
+                }
+            }
+           $connessione = null;
+            } catch(PDOException $e){
+                die("Errore nella gestione del database $db: " . $e->getMessage());
             }
         }
-       $connessione = null;
-        } catch(PDOException $e){
-            die("Errore nella gestione del database $db: " . $e->getMessage());
-        }
-    }
-    ?>
-    </div>
+        ?>
+        </div>
+    </body>
 </html>

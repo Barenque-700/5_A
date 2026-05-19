b84443ec78903f421f70afe633527fdad9bb7adf
Adday Yasser <adday.yasser7@gmail.com>
2026-05-19 09:29:55 +0200

    Update eventi.php

--- eventi.php
+++ eventi.php
@@ -60,7 +60,7 @@ if ($accesso != 1) {
             <div class="mt-3">
                 <li class="nav-item"><a class="nav-link px-3" href="Asteria.php"><i class="fa fa-house me-2"></i>Home</a></li>
                 <li class="nav-item"><a class="nav-link px-3" href="eventi.php" style="color: var(--primary-color) !important;"><i class="fa-solid fa-calendar-days me-2"></i>Eventi</a></li>
-                <li class="nav-item"><a class="nav-link px-3" href="Asteria.php"><i class="fa-solid fa-bell me-2"></i>Notifiche</a></li>
+                <li class="nav-item"><a class="nav-link px-3" href="notifiche.php"><i class="fa-solid fa-bell me-2"></i>Notifiche</a></li>
                 <li class="nav-item"><a class="nav-link px-3" href="Ricerca.php"><i class="fa-solid fa-magnifying-glass me-2"></i>Ricerca</a></li>
             </div>
         </div>
